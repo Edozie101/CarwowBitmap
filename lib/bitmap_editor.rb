726d9efa39af6cc52949bf955f9c2d81a1505953
Edozie Izegbu <edozieizegbu@unknown.home>
2020-02-04 00:12:41 +0000

    added more failsafes

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -1,9 +1,10 @@
 class BitmapEditor
-    attr_accessor :matrix,:blankmatrix
+    attr_accessor :matrix,:blankmatrix,:error
   
     def initialize
       @matrix = []
       @blankmatrix = []
+      
 
     end
   
@@ -12,6 +13,7 @@ class BitmapEditor
     return puts "please provide correct file" if file.nil? || !File.exists?(file)
 
     File.open(file).each do |line|
+      errorcheck(matrix)
       line = line.split(" ")
       case line.first()
       when 'I'
@@ -30,9 +32,7 @@ class BitmapEditor
       when 'C'
           self.matrix = self.blankmatrix
       when 'S'
-           self.matrix.each do |n|
-            puts n 
-           end
+           puts self.matrix.to_s
       else
           puts line.to_s 
       end
@@ -40,34 +40,52 @@ class BitmapEditor
   end
 
   def create_matrix(x,y,m=[]) 
-      x = x.to_i  ; y = y.to_i ; matrix = m
+      x = x.to_i  
+      y = y.to_i
+      matrix = m
+      err = nil
       begin
         assert_incorrect_barrier?(x,y)
       rescue  ArgumentError => e
+         err = e.message
          puts e.message()
       end
-      r=0
-      y.times do
-        matrix.push([])
-        x.times do
-          matrix[r].push("w")
+      if err.nil?
+        r=0
+        y.times do
+          matrix.push([])
+          x.times do
+            matrix[r].push("o")
+          end
+          r +=1
         end
-        r +=1
+        return matrix
+      else
+        return e
+    
       end
-      return matrix
   end
 
   def change_x_y_value(x,y,colour,m)
     x = x.to_i
     y = y.to_i
+    matrix = m  
+    err = nil
     begin
+      puts "changing xy"
+      puts matrix.class
       assert_size_mismatch?(x,y,m)
     rescue  ArgumentError => e
+       err = e
        puts e.message()
     end
-     matrix = m     
-     matrix[y-1][x-1] = colour
-     return matrix
+     if err.nil?
+   
+      matrix[y-1][x-1] = colour
+      return matrix
+     else 
+      return err
+     end
 
   end
 
@@ -77,10 +95,24 @@ class BitmapEditor
     y1 = y1.to_i
     y2 = y2.to_i
     matrix = m
-    (y1..y2).each do |n|
-      matrix[n-1][x-1] = colour
+    err = nil
+    begin
+      puts "change y value func"
+      puts matrix.class
+      assert_size_ys_mismatch?(x,y1,y2,matrix)
+    rescue  ArgumentError => e
+      err = e
+       puts e.message()
     end
-    return matrix
+
+    if err.nil?
+      (y1..y2).each do |n|
+        matrix[n-1][x-1] = colour
+      end
+      return matrix
+    else 
+      return err
+    end
   end
 
   def change_x_value(x1,x2,y, colour, m)
@@ -88,22 +120,39 @@ class BitmapEditor
     x1 = x1.to_i
     x2 = x2.to_i
     matrix = m
-    (x1..x2).each do |n|
-        matrix[y-1][n-1] = colour
+    err = nil
+    begin
+      puts "in x change value"
+      assert_size_xs_mismatch?(x1,x2,y,m)
+    rescue ArgumentError => e
+        err = e
+        puts e.message
     end
-    return matrix
+    if err.nil?
+      (x1..x2).each do |n|
+          matrix[y-1][n-1] = colour
+      end
+      return matrix
+    else 
+      return e
+    end
   end
 
   def assert_size_mismatch?(x,y,m)
       supy = m.length 
       supx = m[0].length
+      if m.nil? or x.nil? or y.nil? 
+        raise ArgumentError.new("NilValue ")
+
+      end
+     
 
       if (x > supx)
         raise ArgumentError.new("The X value is too large ")
       elsif (y > supy)
          raise ArgumentError.new("The Y value is too large ")
       else
-        puts "Size is all good"
+        puts "Size is all good xy"
       end 
   end
 
@@ -111,26 +160,53 @@ class BitmapEditor
     supy = m.length 
     supx = m[0].length
 
-    if (x2 > supx || x1 < 0)
-      raise ArgumentError.new("The X value is too large ")
+    if (x2 > supx || x2 <= 0)
+      raise ArgumentError.new("The X2 value is too large ")
+    elsif (x1 > supx || x1 > supx || x1 > x2 )
+      raise ArgumentError.new("The X1 value is too large")
     elsif (y > supy)
        raise ArgumentError.new("The Y value is too large ")
     else
-      puts "Size is all good"
+      puts "Size is all good X"
     end 
-end
+  end
+
+  def assert_size_ys_mismatch?(x,y1,y2,m)
+    puts "y mismatch" 
+    puts m.length
+
+    supy = m.length 
+    supx = m[0].length
+
+    if (y2 > supy || y2 <= 0)
+      raise ArgumentError.new("The Y2 value is too large ")
+    elsif (y1 > supy || y1 > supy ||  y1 > y2)
+      raise ArgumentError.new("The Y1 value is too large ")
+    elsif (x > supy)
+       raise ArgumentError.new("The X value is too large ")
+    else
+      puts "Size is all good Y "
+    end 
+  end
 
 
   def assert_incorrect_barrier?(x,y)
     supy =  250
     supx =  250
 
-    if (x > supx || x < 0 )
+    if (x > supx || x <= 0 )
       raise ArgumentError.new("The X value is out of bounds")
-    elsif (y > supy || y < 0)
+
+    elsif (y > supy || y <= 0)
        raise ArgumentError.new("The Y value is out of bounds ")
     else
-      puts "Size is all good"
+      puts "Size is all good create"
     end 
+  end
+
+  def errorcheck(e)
+      if e.class == ArgumentError
+         abort("Huston we have a problem " + e.message)
+      end
   end
 end
